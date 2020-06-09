0ed6a509ca5d42b8f01c4a8364a283ff34ddee07
Aly Sivji <alysivji@gmail.com>
2020-06-08 20:14:08 -0500

    clean up version and description

--- finite_state_machine/__init__.py
+++ finite_state_machine/__init__.py
@@ -1,6 +1,6 @@
-"""General purpose toolbox"""
+"""Lightweight, decorator-based Python implementation of a Finite State Machine"""
 
-__version__ = '0.1.0'
+__version__ = "0.1.1"
 
 
 from .state_machine import StateMachine, transition  # noqa
