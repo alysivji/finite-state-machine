cfb318b47a808099bcf8a766369fcade7741beda
Aly Sivji <alysivji@gmail.com>
2020-12-07 07:12:15 -0600

    Bump version to 0.5.0 (#26)

--- finite_state_machine/__init__.py
+++ finite_state_machine/__init__.py
@@ -1,6 +1,6 @@
 """Lightweight, decorator-based implementation of a Finite State Machine"""
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 
 from .state_machine import StateMachine, transition  # noqa
